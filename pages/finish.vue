e2765597688eca81f472d2610c1bbd55ad2dac85
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-07-28 19:11:54 +0800

    finished页面

--- pages/finish.vue
+++ pages/finish.vue
@@ -4,7 +4,24 @@
     <div class="cont">
       <div class="strip_box" v-for="(item, index) in finished" :key="index">
         <div class="strip">{{ index + 1 }}. {{ item.title }}</div>
-        <div class="finish">{{ item.finished }}</div>
+        <div class="finish" v-show="item.finished == false">
+          {{ item.finished }}
+        </div>
+        <div class="finished" v-show="item.finished == true">
+          {{ item.finished }}
+        </div>
+      </div>
+    </div>
+    <div class="create_task">
+      <div class="title">创建任务</div>
+      <div class="create_box">
+        <van-field
+          v-model="taskCont"
+          label="输入任务内容"
+          placeholder="请输入任务内容.."
+        />
+        <van-button style="margin-right: 10px;" type="primary" size="small" @click="create">创建</van-button>
+        <van-button type="info" size="small" @click="taskCont == ''">重置</van-button>
       </div>
     </div>
   </div>
@@ -27,7 +44,7 @@ export default {
         },
         {
           title: "使用Vant2",
-          finished: false,
+          finished: true,
         },
         {
           title: "国际化文本缩放组件可用性",
@@ -37,10 +54,31 @@ export default {
           title: "虚拟列表",
           finished: false,
         },
+        {
+          title: "将该列表写入数据库",
+          finished: false,
+        },
+        {
+          title: "添加权限，只有admin可编辑",
+          finished: false,
+        },
       ],
+      taskCont: "",
     };
   },
-  mounted() {},
+  methods: {
+    create() {
+      if (this.taskCont == "") {
+        this.$toast("请输入任务内容");
+      } else {
+        this.finished.push({
+          title: this.taskCont,
+          finished: false,
+        });
+        this.taskCont = "";
+      }
+    }
+  }
 };
 </script>
   
@@ -53,6 +91,8 @@ export default {
   }
   .cont {
     width: 100%;
+    height: 70vh;
+    overflow-y: scroll;
     .strip_box {
       width: 100%;
       display: flex;
@@ -63,6 +103,30 @@ export default {
       }
       .finish {
         width: 20%;
+        color: orangered;
+      }
+      .finished {
+        width: 20%;
+        color: blue;
+      }
+    }
+  }
+  .create_task {
+    margin-top: 20px;
+    width: 78vw;
+    position: fixed;
+    bottom: 0;
+    .title {
+      font-size: 23px;
+      font-weight: 700;
+    }
+    .create_box {
+      width: 100%;
+      display: flex;
+      align-items: center;
+
+      :deep(.van-cell) {
+        width: 80%;
       }
     }
   }
--- plugins/vant.js
+++ plugins/vant.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import { Button, Toast } from 'vant';
+import { Field } from 'vant'
 import 'vant/lib/index.css';
 
 Vue.use(Button);
 Vue.use(Toast);
+Vue.use(Field);
